166c50cd8ebf4145b513b14c431829720831f361
Jean <jean_henrique2@hotmail.com>
2025-07-28 13:31:10 -0300

    function for the buttons

--- todo/src/components/Todo.jsx
+++ todo/src/components/Todo.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 
-const todo = ({todo}) => {
+const todo = ({todo, removeTodo, completeTodo}) => {
   return (
-    <div className="todo">
+    <div className="todo" 
+    style={{textDecoration: todo.iscompleted ? "line-through" : ""}}>
     <div className="content">
 <p>{todo.text}</p>
 <p className="category">({todo.category})</p>
 </div>
 <div>
-  <button className="complete">Completar</button>
-  <button className='remove'>Excluir</button>
+  <button className="complete" onClick={()=> completeTodo(todo.id)}>
+    Completar
+    </button>
+  <button className='remove' onClick={()=> removeTodo(todo.id)}>Excluir</button>
 </div>
  </div>
   )
