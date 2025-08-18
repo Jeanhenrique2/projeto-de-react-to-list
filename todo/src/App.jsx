8db8618ae471f1ac7500e7215c69a32a933fdbf8
Jean <jean_henrique2@hotmail.com>
2025-08-18 15:17:33 -0300

    Creanting filter

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -4,6 +4,7 @@ import './app.css';
 import Todo from './components/Todo';
 import TodoForm from './components/TodoForm';
 import Search from "./components/Search";
+import Filter from './components/filter';
 
 
 function App() {
@@ -29,6 +30,9 @@ function App() {
   ]);
 
   const [search, setSearch] = useState("");
+
+  const [filter, setFilter] = useState("All");
+  const [sort, setSort] = useState("Asc");
 
   const addTodo = (text, category) => {
 
@@ -61,12 +65,24 @@ function App() {
   <div className="app">
     <h1>Lista de tarefas</h1>
     <Search search={search} setSearch={setSearch} />
+    <Filter filter={filter} setFilter={setFilter} setSort={setSort}/>
     <div className="todo-list">
 
       {todos
+      .filter((todo)=> 
+        filter==="All"
+      ? true 
+      : filter=="Completed" 
+      ? todo.iscompleted: 
+      !todo.iscompleted
+    )
       .filter((todo) => 
         todo.text.toLowerCase().includes(search.toLowerCase())
       )
+      .sort((a,b)=> sort=== "Asc"
+      ? a.text.localeCompare(b.text) 
+      :b.text.localeCompare(a.text) 
+    )
       .map((todo) => (
         <Todo 
         key={todo.id} 
